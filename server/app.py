bc55a3703b229b193994bada32d9a7fa9dae0f99
samuelgitau <amsamgitau@gmail.com>
2024-11-05 23:12:17 +0300

    modified files

--- server/app.py
+++ server/app.py
@@ -3,7 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder='../client/dist',
+    static_url_path='',
+    template_folder='../client/dist',
+    
+    )
 db = SQLAlchemy()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://my_database_7z4p_user:irdDxXIVuOJrPFrVAbRNiW5Aev4O2D32@dpg-csfsmjdsvqrc739r5lvg-a.oregon-postgres.render.com/google_drive_db'
