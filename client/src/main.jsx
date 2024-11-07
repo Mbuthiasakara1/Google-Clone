1eba0007987406d9df660c67e92017fb6e7a50ad
kevin <kipronok234@gmail.com>
2024-11-08 01:15:58 +0300

    login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import routes from './components/Routes'
+import { SnackbarProvider } from 'notistack'
 
-import App from './App.jsx'
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <SnackbarProvider 
+      maxSnack={3} 
+      autoHideDuration={10000}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+    >
+      <RouterProvider router={router} />
+    </SnackbarProvider>
+  </StrictMode>
 )
