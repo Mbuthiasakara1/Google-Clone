32af9661025fedaa34c8519bd76e5707600e3789
kevin <kipronok234@gmail.com>
2024-11-08 01:24:33 +0300

    combined login with signup and nav bar

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import './Login.css'
+
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
@@ -17,16 +17,7 @@ function Login() {
             <input type="email" placeholder="Email " required />
             <input type="password" placeholder="Password " required />
           </div>
-          {/* <div className="forgot-link">
-            <a href="#" onClick={(e) => { e.preventDefault(); navigate('/emailrecovery'); }}>Forgot email?</a>
-          </div>
-          <div className="guest-mode-info">
-            <p>
-              Not your computer? Use Guest mode to sign in privately.
-              <br />
-              <a href="#">Learn more</a>
-            </p>
-          </div> */}
+          
           <button type="submit" className="login-button">login</button>
         </form>
         
