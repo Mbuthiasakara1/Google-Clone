6182771fd9fc23c39b55156d79b4e8b281b8a2a8
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-11-07 20:28:27 +0300

    nav-bar updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Header from "./components/Header";
+import styled, { ThemeProvider } from "styled-components";
 
+const lightTheme = {
+  background: "#fff",
+  color: "#000",
+};
+const darkTheme = {
+  background: "#333",
+  color: "#fff",
+};
+const AppContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.color};
+`;
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+        <AppContainer>
+          <div style={{ flex: 1 }}>
+            <Header toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+          </div>
+        </AppContainer>
+      </ThemeProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
