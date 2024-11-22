ebcab9fcbadbda4a20e90b053e90f59bcd49ce8a
samuelgitau <amsamgitau@gmail.com>
2024-11-22 16:11:12 +0300

    modified vite.config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,7 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       'https://google-drive-clone-v6g6.onrender.com/api': {
-//         target: '', 
-//         changeOrigin: true,
-//         secure: false,
-//         rewrite: (path) => path.replace(/^\api/, ''),
-//       },
-//     },
-//   },
-//   plugins: [react()],
-// });
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
